6723a0f14872edad9cca227d617bca8644b6259b
overflowed-stack <spaydermendima@gmail.com>
2022-10-17 22:47:44 -0700

    Add full solutions for natural numbers tasks

--- Natural2.py
+++ Natural2.py
@@ -8,13 +8,39 @@ def primes(n):
             sieve[i*i::2*i]=[False]*((n-i*i-1)//(2*i)+1)
     return [2] + [i for i in range(3,n,2) if sieve[i]]
 
-MAX = 100000
+def pairEqNotPair(number):
+    if number == 0:
+        return False
+    pair = 0
+    notPair = 0
+    while number != 0:
+        if number % 2 == 0:
+            pair += 1
+        else:
+            notPair += 1
+        number = int(number/10)
+    return pair == notPair
+
+filtered = lambda x: [x[i] for i in range(0, len(x)) if pairEqNotPair(x[i])]
+
+def getNumbersAfter(nNumsAfter, primesBeforeNumber):
+    nthWithoutStart = nNumsAfter + len(primesBeforeNumber)
+    estimateNthWithoutStart = math.ceil(nthWithoutStart*math.log(nthWithoutStart) + nthWithoutStart*math.log(math.log(nthWithoutStart)))
+    return primes(estimateNthWithoutStart)[len(primesBeforeNumber):nthWithoutStart]
+
 num = 47
-primesBeforeNum = primes(num + 1)
-pair = 0
-nonPair = 0
 nth = 10
-nthWithoutStart = nth + len(primesBeforeNum)
-estimateNthWithoutStart = math.ceil(nthWithoutStart*math.log(nthWithoutStart) + nthWithoutStart*math.log(math.log(nthWithoutStart)))
-res = primes(estimateNthWithoutStart)[len(primesBeforeNum):nthWithoutStart]
-print(res)
+
+primesBeforeNum = primes(num + 1)
+
+result = []
+nthTemp = nth
+while True:
+    result = filtered(getNumbersAfter(nthTemp, primesBeforeNum))
+    if (len(result) < nth):
+        nthTemp += 20
+    else:
+        result = result[0:nth+1]
+        break
+
+print(result)
--- Natural3.py
+++ Natural3.py
@@ -8,13 +8,36 @@ def primes(n):
             sieve[i*i::2*i]=[False]*((n-i*i-1)//(2*i)+1)
     return [2] + [i for i in range(3,n,2) if sieve[i]]
 
-MAX = 100000
+def notPair(number):
+    if number == 0:
+        return False
+    pair = 0
+    while number != 0:
+        if number % 2 == 0:
+            pair += 1
+        number = int(number/10)
+    return pair == 0
+
+filtered = lambda x: [x[i] for i in range(0, len(x)) if notPair(x[i])]
+
+def getNumbersAfter(nNumsAfter, primesBeforeNumber):
+    nthWithoutStart = nNumsAfter + len(primesBeforeNumber)
+    estimateNthWithoutStart = math.ceil(nthWithoutStart*math.log(nthWithoutStart) + nthWithoutStart*math.log(math.log(nthWithoutStart)))
+    return primes(estimateNthWithoutStart)[len(primesBeforeNumber):nthWithoutStart]
+
 num = 47
-primesBeforeNum = primes(num + 1)
-pair = 0
-nonPair = 0
 nth = 10
-nthWithoutStart = nth + len(primesBeforeNum)
-estimateNthWithoutStart = math.ceil(nthWithoutStart*math.log(nthWithoutStart) + nthWithoutStart*math.log(math.log(nthWithoutStart)))
-res = primes(estimateNthWithoutStart)[len(primesBeforeNum):nthWithoutStart]
-print(res)
+
+primesBeforeNum = primes(num + 1)
+
+result = []
+nthTemp = nth
+while True:
+    result = filtered(getNumbersAfter(nthTemp, primesBeforeNum))
+    if (len(result) < nth):
+        nthTemp += 20
+    else:
+        result = result[0:nth+1]
+        break
+
+print(result)
